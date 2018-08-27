d968c6cad16fc715f8d75896d6dcea788075dca8
Heidi Kasemir <heidi.kasemir@gmail.com>
2018-08-26 17:05:26 -0700

    include content on about and contact pages

--- src/views/about.js
+++ src/views/about.js
@@ -1,5 +1,8 @@
 import React, { PureComponent } from 'react';
 import { SITE_TITLE } from '../helpers/constants';
+import vanImg from '../images/van.jpg';
+import securityImg from '../images/security-camera.jpg';
+import gateImg from '../images/gate.jpg';
 
 export default class About extends PureComponent {
   componentDidMount() {
@@ -8,8 +11,27 @@ export default class About extends PureComponent {
 
   render() {
     return (
-      <section>
+      <section className="page-content">
         <h1>About Us</h1>
+        <img src={vanImg} alt="van in the aisle of our facility" />
+        <p>
+          As a family owned small business, we care about each customer and
+          bringing you the best service possible. We took ownership of
+          AAAffordable in 2009 and have since turned the once ailing property
+          into a thriving business. Our mission is to provide a high tech and
+          secure Self Storage facility with excellent service for local
+          residents and businesses. We have an "A+" rating with the BBB and are
+          a member of the Frederick Chamber of Commerce.
+        </p>
+        <h2>Our Security</h2>
+        <img src={securityImg} alt="security camera" />
+        <p>
+          Our security system includes 24/7 security camera footage and an
+          access gate with a coded keypad. We are always a quick phone call away
+          whenever you have questions. If you need assistance with the gate
+          operation check out this short YouTube video.
+        </p>
+        <img src={gateImg} alt="our electronic gate" />
       </section>
     );
   }
--- src/views/contact.js
+++ src/views/contact.js
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react';
 import { SITE_TITLE } from '../helpers/constants';
+import wolframImg from '../images/wolfram.jpg';
+import lauraImg from '../images/laura.jpg';
 
 export default class Contact extends PureComponent {
   componentDidMount() {
@@ -8,8 +10,23 @@ export default class Contact extends PureComponent {
 
   render() {
     return (
-      <section>
+      <section className="page-content">
         <h1>Contact Us</h1>
+        <p>Let us know if you have any questions</p>
+        <figure>
+          <img src={wolframImg} alt="portrait of Wolfram, the manager" />
+          <figcaption>Wolfram - Manager</figcaption>
+          <p>
+            Email: <a href="mailto:wk@aaafford.com">wk@aaafford.com</a>
+          </p>
+        </figure>
+        <figure>
+          <img src={lauraImg} alt="portrait of Laura, the assistant manager" />
+          <figcaption>Laura - Assistant Manager</figcaption>
+          <p>
+            Email: <a href="mailto:laura@aaafford.com">laura@aaafford.com</a>
+          </p>
+        </figure>
       </section>
     );
   }
